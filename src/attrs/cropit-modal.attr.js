29df5e694da8359b105883884198b45a5e724e29
Marc Godard <me@marcgodard.com>
2018-03-09 16:34:59 -0500

    Only initiate cropit once.

--- src/attrs/cropit-modal.attr.js
+++ src/attrs/cropit-modal.attr.js
@@ -4,42 +4,49 @@ import 'cropit'
 
 callback.attr('cropit-modal', (el) => {
   let $el = $(el)
+  let init = false
 
   $el.on('shown.bs.modal', function () {
     let $cropit = $el.find('#image-cropper')
 
-    $cropit.cropit({
-      exportZoom: 2,
-      maxZoom: 3,
-      imageBackground: true,
-      imageBackgroundBorderWidth: 5
-    })
+    console.log($cropit.attr('data-init'))
 
-    let current = $cropit.attr('data-current-image')
-    if (current !== 'undefined' && current !== '') {
-      $cropit.cropit('imageSrc', current)
-    }
+    if (!init) {
+      init = true
 
-    $('.select-image-btn').click(function () {
-      $el.find('.cropit-image-input').click()
-    })
-
-    $('.rotate-cw-btn').click(function () {
-      $cropit.cropit('rotateCW')
-    })
-
-    $('.rotate-ccw-btn').click(function () {
-      $cropit.cropit('rotateCCW')
-    })
-
-    $cropit.on('get-data', function () {
-      let data = $cropit.cropit('export', {
-        type: 'image/jpeg',
-        quality: 0.9
+      $cropit.cropit({
+        exportZoom: 2,
+        maxZoom: 3,
+        imageBackground: true,
+        imageBackgroundBorderWidth: 5
       })
 
-      $cropit.attr('data-image-data', data)
-      $cropit.attr('data-current-image', data)
-    })
+      let current = $cropit.attr('data-current-image')
+      if (current !== 'undefined' && current !== '') {
+        $cropit.cropit('imageSrc', current)
+      }
+
+      $('.select-image-btn').click(function () {
+        $el.find('.cropit-image-input').click()
+      })
+
+      $('.rotate-cw-btn').click(function () {
+        $cropit.cropit('rotateCW')
+      })
+
+      $('.rotate-ccw-btn').click(function () {
+        $cropit.cropit('rotateCCW')
+      })
+
+      $cropit.on('get-data', function () {
+        let data = $cropit.cropit('export', {
+          type: 'image/jpeg',
+          quality: 0.9
+        })
+
+        $cropit.attr('data-image-data', data)
+        $cropit.attr('data-current-image', data)
+      })
+    }
   })
 })
